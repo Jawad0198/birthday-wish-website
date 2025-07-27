cf232972c9a5e82c5843073b2badd52e1d4ab9a1
jawad0198 <jm03401045@gmail.com>
2025-07-27 00:44:35 -0700

    full-wordkin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["ybvzdvqv1orpf8io.public.blob.vercel-storage.com"], // 👈 Add your blob domain here
+  },
 };
 
 export default nextConfig;
